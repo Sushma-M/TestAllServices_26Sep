7ddb50286ff9014739feab6df7ce8b90403f9bc5
Naga Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-09-26 17:54:16 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "neon",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestAllServicesforVCS_31May_PRd1",
+  "homePage" : "Main",
+  "name" : "TestAllServicesforVCS_31May_PRd1",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
